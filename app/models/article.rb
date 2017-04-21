48d8346502db14afa274226e30e26d1bf3641d46
Mitcheljager <mitchel_jager@hotmail.com>
2017-04-21 10:57:49 +0200

    Added basic elasticsearch

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
-class Article < ApplicationRecord
+
+require 'elasticsearch/model'
+
+class Article < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 end
+
+Article.import force:true
