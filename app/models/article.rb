c3c2a00b761769ab878143fbb77ed55bee8ff7b5
Mitcheljager <mitchel_jager@hotmail.com>
2017-04-21 11:17:15 +0200

    Added search weight and mapping

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,30 @@ require 'elasticsearch/model'
 class Article < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'text']
+          }
+        }
+      }
+    )
+  end
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english'
+      indexes :text, analyzer: 'english'
+    end
+  end
 end
 
+Article.__elasticsearch__.client.indices.delete index: Article.index_name rescue nil
+Article.__elasticsearch__.client.indices.create \
+  index: Article.index_name,
+  body: { settings: Article.settings.to_hash, mappings: Article.mappings.to_hash }
 Article.import force:true
